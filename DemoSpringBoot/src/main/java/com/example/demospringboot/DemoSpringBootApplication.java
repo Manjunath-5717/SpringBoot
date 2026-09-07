a906ea0c752812d920279c89791cbcfc18fa87f3
Manjunath <manjunathragati704@gmail.com>
2026-09-07 22:18:41 +0530

    This is Simple Spring Boot project

---
+++ DemoSpringBoot/src/main/java/com/example/demospringboot/DemoSpringBootApplication.java
@@ -0,0 +1,21 @@
+package example.demospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpingBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingBootApplication.class, args);
+
+    }
+    @GetMapping
+    public String helloworld(){
+        return "hello world Spring boot";
+    }
+
+}
